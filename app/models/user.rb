cafcfebd0488c442df89fd609549ac7f6174ccd1
TKIN10 <ace.1031.3d2y@icloud.com>
2023-01-20 16:23:47 +0900

    新規登録機能、ログイン、ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_katakana, presence: true
+  validates :first_name_katakana, presence: true
+  validates :birthday, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 end
