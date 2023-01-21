c58eb0ae3668e1874512e9203d1eb81866518f17
TKIN10 <ace.1031.3d2y@icloud.com>
2023-01-21 20:48:51 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
+  has_one :category
+  has_one :condition
+  has_one :delivery_charge
+  has_one :area
+  has_one :number_of_day
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
 end
