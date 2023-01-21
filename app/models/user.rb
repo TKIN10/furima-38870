3732736307b3ae4733d06cac3e3f522ea76f7767
TKIN10 <ace.1031.3d2y@icloud.com>
2023-01-21 15:20:06 +0900

    ユーザー管理機能(修正分)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true,format: {with: /\A[ぁ-んァ-ヶ一\p{Han}]+\z/ }
-  validates :first_name, presence: true,format: {with: /\A[ぁ-んァ-ヶ一\p{Han}]+\z/ }
+  validates :last_name, presence: true,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, presence: true,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :last_name_katakana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
   validates :first_name_katakana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
