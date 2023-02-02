bf1015bc1c9a81d575c2d1984d74c6f977dab527
TKIN10 <ace.1031.3d2y@icloud.com>
2023-02-02 17:38:28 +0900

    フォームの情報を保存

--- app/controllers/purchase_informations_controller.rb
+++ app/controllers/purchase_informations_controller.rb
@@ -9,6 +9,7 @@ class PurchaseInformationsController < ApplicationController
   def create
     @purchase_information_shipping_address = PurchaseInformationShippingAddress.new(shipping_address_params)
     if  @purchase_information_shipping_address.valid?
+      pay_item
       @purchase_information_shipping_address.save
       redirect_to root_path
     else
@@ -19,10 +20,20 @@ class PurchaseInformationsController < ApplicationController
   private
 
   def shipping_address_params
-    params.require(:purchase_information_shipping_address).permit(:area_id, :postal_code, :municipality, :adress, :bulding_name, :number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:purchase_information_shipping_address).permit(:area_id, :postal_code, :municipality, :adress, :bulding_name, :number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
   def set_item
     @item = Item.find(params[:item_id])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.amount_of_money,
+      card: shipping_address_params[:token],
+      currency: 'jpy'
+    )
+  end
+
 end
--- app/models/purchase_information_shipping_address.rb
+++ app/models/purchase_information_shipping_address.rb
@@ -1,17 +1,18 @@
 class PurchaseInformationShippingAddress
   include ActiveModel::Model
-  attr_accessor :area_id, :postal_code, :municipality, :adress, :bulding_name, :number, :user_id, :item_id
+  attr_accessor :area_id, :postal_code, :municipality, :adress, :bulding_name, :number, :user_id, :item_id, :token
 
-  validates :area_id, presence: true,format: { with: /\A\d{3}[-]\d{4}\z/ }
-  validates :postal_code, presence: true
+  validates :area_id, presence: true, numericality: { other_than: 1 }
+  validates :postal_code, presence: true,format: { with: /\A\d{3}[-]\d{4}\z/ }
   validates :municipality, presence: true
   validates :adress, presence: true
   validates :bulding_name, presence: true
   validates :number, presence: true,format: { with: /\A\d{10,11}\z/ }
+  validates :token, presence: true
 
   def save
     purchase_information = PurchaseInformation.create(user_id: user_id, item_id: item_id)
 
-    ShippingAddress.create(area_id: area_id, postal_code:postal_code, municipality:municipality, adress:adress, bulding_name:bulding_name, number:number)
+    ShippingAddress.create(area_id:area_id, postal_code:postal_code, municipality:municipality, adress:adress, bulding_name:bulding_name, number:number, purchase_information_id:purchase_information.id)
   end
 end
