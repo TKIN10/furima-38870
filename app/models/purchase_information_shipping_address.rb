3a1241c6543c05c2b5c9b20daa929ef79f7879c4
TKIN10 <ace.1031.3d2y@icloud.com>
2023-02-03 15:53:15 +0900

    with_optionsでコードをまとめる

--- app/models/purchase_information_shipping_address.rb
+++ app/models/purchase_information_shipping_address.rb
@@ -2,14 +2,16 @@ class PurchaseInformationShippingAddress
   include ActiveModel::Model
   attr_accessor :area_id, :postal_code, :municipality, :adress, :bulding_name, :number, :user_id, :item_id, :token
 
-  validates :area_id, presence: true, numericality: { other_than: 1 }
-  validates :postal_code, presence: true,format: { with: /\A\d{3}[-]\d{4}\z/ }
-  validates :municipality, presence: true
-  validates :adress, presence: true
-  validates :number, presence: true,format: { with: /\A\d{10,11}\z/ }
-  validates :token, presence: true
-  validates :item_id, presence: true
-  validates :user_id, presence: true
+  with_options presence: true do
+    validates :area_id, numericality: { other_than: 1 }
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :municipality
+    validates :adress
+    validates :number, format: { with: /\A\d{10,11}\z/ }
+    validates :token
+    validates :item_id
+    validates :user_id
+  end
 
   def save
     purchase_information = PurchaseInformation.create(user_id: user_id, item_id: item_id)
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,15 @@ class User < ApplicationRecord
   has_many :items
   has_many :shipping_addresses
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-  validates :first_name, presence: true,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-  validates :last_name_katakana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_katakana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :last_name_katakana, format: {with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_katakana,  format: {with: /\A[ァ-ヶー－]+\z/ }
+    validates :birthday
+  end
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 end
